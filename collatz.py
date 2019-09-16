78149f8a1cc0e1ec9d500ad9888d0fba89d2901a
Mark Cotter <g00376335@gmit.ie>
2019-09-16 19:12:58 +0100

    Second draft of collatz done.

--- collatz.py
+++ collatz.py
@@ -1,5 +1,5 @@
 # The number we will perform the Collatz operation on.
-n = 20
+n = int(input("Please enter a positive integer: "))
 
 # Keep looping until we reach 1.
 # Note: this assumes the Collatz conjecture is true.
@@ -7,10 +7,10 @@ while n != 1:
     print(n)
     if n % 2 == 0:
         # If n is even, divide it by two.
-        n = n / 2
+        n = n // 2
     else:
         # If n is odd, multiple it by three and add 1.
         n = (3 * n) + 1
-        
+
 # Finally, print the 1.
 print(n)
